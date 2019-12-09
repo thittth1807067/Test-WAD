66766e40d9fa62eb3615a9d7c39e9f5696b59d10
unknown <thittth1807067@fpt.edu.vn>
2019-12-09 17:15:34 +0700

    test

---
+++ Test-WAD/Models/Exam.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Test_WAD.Models
+{
+    public class Exam
+    {
+        
+        public int Id { get; set; }
+        public string ExamSubject { get; set; }
+      
+        public string StartTime { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
+        public string ExamDate { get; set; }
+        [DataType(DataType.DateTime)]
+
+        [DisplayFormat(DataFormatString = "{0:dd/M/yyyy}", ApplyFormatInEditMode = true)]
+        public string ExamDuration { get; set; }
+        public string ClassRoom{ get; set; }
+        public string Faculty { get; set; }
+        public string Status { get; set; }
+    }
+   
+}
---
+++ Test-WAD/Models/Test_WADContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Test_WAD.Models
+{
+    public class Test_WADContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Test_WADContext() : base("name=Test_WADContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Test_WAD.Models.Exam> Exams { get; set; }
+    }
+}
